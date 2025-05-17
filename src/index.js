bf3c2f12330435acbe7564eb53c3da0785ac3063
Ritvik Jain <jritvik007@gmail.com>
2025-05-17 18:26:40 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -5,31 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { useSelector } from 'react-redux';
 
 
-const AppWithTheme = () => {
-  const mode = useSelector((state) => state.theme?.mode); 
 
-  const muiTheme = createTheme({
-    palette: {
-      mode: mode || 'light', 
-    },
-  });
-
-  return (
-    <ThemeProvider theme={muiTheme}>
-      <App />
-    </ThemeProvider>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <AppWithTheme />
+      <App />
     </Provider>
   </React.StrictMode>
 );
